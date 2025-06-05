86f40268a34a3a0bc629035c0c2d42a3efb2527a
Alyssa <a.c.fusato@gmail.com>
2025-06-05 13:56:08 -0500

    Removed references to diagram flows

--- code.ts
+++ code.ts
@@ -4,8 +4,6 @@ var nodes: SceneNode[] = [];
 var filterednodes: SceneNode[] = [];
 var alphabeticalLabel = false;
 var appendLabel = false;
-var flowDiagram = false;
-var diagramArray = Array.from(Array(2), () => new Array(4));
 
 function convertToLetter(n: number) {
   if (n < 27) { let letter = (n + 9).toString(36).toUpperCase(); return letter; }
@@ -45,60 +43,12 @@ function positionSort(a: any, b: any) { // evalutates which node is either above
   return 0;
 }
 
-// function uniq(a) {
-//     return a.filter(function(item, pos, ary) {
-//         return !pos || item != ary[pos - 1];
-//     });
-// }
-
-function diagramSort() {
-  // dilemma- we need a sort function, but I also want to grab anything in a column (x is same, y is different) and throw them in a different array, which itself is sorted...
-  // currently trying to first run the normal sort, then 
-  let uniqueXVals = []; //FIND ALL WITH SAME X VALUES
-  // Find all possible X values, then find out which ones share them
-  
-  for (let x = 0; x < (nodes.length - 1); x++) {
-    let currentNode = nodes[x].x;
-    console.log(currentNode);
-
-    uniqueXVals.push(currentNode);
-    // if(!uniqueXVals.find(currentNode)){}
-    // for(let v = 1; v < (uniqueXVals.length - 1); v++){
-    //   if(uniqueXVals[x].x == uniqueXVals[v].x) {
-    //     console.log("Found matching values!");
-    //     console.log(uniqueXVals[v]);
-    //   }
-    // }
-    // const transformA = nodes[x].absoluteTransform as Transform;
-    // const ax = transformA[0][2];
-    // // const ay = transformA[1][2];
-    // const transformB = nodes[x + 1].absoluteTransform as Transform;
-    // const bx = transformB[0][2];
-    // // const by = transformB[1][2];
-    // // console.log(ax + " | " + bx);
-
-    // if (ax == bx) {
-    //   console.log(nodes[x] + " horizontal matches " + nodes[x]);
-    //   // if (diagramArray.length < 1) {
-    //   //   diagramArray.push([a, b]);
-    //   // } else {
-    //   //   for (let i = 0; i < diagramArray.length; i++) {
-    //   //     if (diagramArray[i].find(a)) { diagramArray[i].push(b); }
-    //   //     else if (diagramArray[i].find(b)) { diagramArray[i].push(a); }
-    //   //     else { diagramArray.push([a, b]); }
-    //   //   }
-    //   // }
-    // }
-  }
-}
-
 const setNameList = async () => { nameList = await figma.clientStorage.getAsync("FrameNames"); }
 
 const checkSelection = async () => { // Filters initial selection for frames and instances that meet requirements
   setNameList();
   if (figma.currentPage.selection.length < 1) { figma.ui.postMessage(false); }
   for (const n of figma.currentPage.selection) { selectionFilters(n); }
-  // diagramSort(); //console.log(diagramArray);
   nodes.sort(positionSort);
   for (const n of nodes) { nodeFilters(n); }
 
@@ -150,10 +100,7 @@ figma.ui.onmessage = (msg: { type: string, name: string, alphabetical: boolean, 
   if (msg.alphabetical == true) { alphabeticalLabel = true; } else { alphabeticalLabel = false; }
   if (msg.append == true) { appendLabel = true; } else { appendLabel = false; }
   if (msg.type === 'add-name') { newName = msg.name; addNewName().then(() => { figma.ui.postMessage(nameList); }); }
-  if (msg.type === 'renumber') { 
-
-    resetArrays(); checkSelection(); 
-  };
+  if (msg.type === 'renumber') { resetArrays(); checkSelection(); };
   if (msg.type === 'clear') { clearClientStorage(); }
   if (msg.type === 'cancel') { figma.closePlugin(); }
 };
