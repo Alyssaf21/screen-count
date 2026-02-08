15c912b9d4804a3af849580f22d5bb91dfc00952
alyssaf21 <a.c.fusato@gmail.com>
2026-02-08 17:59:43 -0500

    Renamed plugin, added Section support, error message fixes, minor ui updates

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 <img width="600" alt="image" src="https://github.com/user-attachments/assets/e82de5e7-d753-4cf3-b2f2-ead25c3215f6" />
 <br> 
 
-### Number your screens in a couple clicks!
+### Update screen numbers in an instant!
 
-_Screen Count_ updates the numbers on your screen labels based on position (top to bottom, left to right). Just add the name of the label container, select the flow or frames you want to update, and hit "Renumber".  
+_Renumber_ updates the numbers on your screen labels based on position (objects at the top are first, then left, right, and bottom). 
+
+Just add the name of the label container, select the flow or frames you want to update, and hit "Renumber".  
 [View in Figma Community](https://www.figma.com/community/plugin/1519388496828481119/screen-count)
 
 - Always starts from 1 (or A, if alphabetical is enabled) and counts up
 - You can now use letters instead of numbers, or choose to append to the existing text instead of replacing it!
+
+Originally published as "Screen Count", renamed for clarity 🧛
 
 If you have any thoughts/suggestions/feedback, I'd love to chat! You can reach me at a.c.fusato@gmail.com
--- code.ts
+++ code.ts
@@ -5,6 +5,8 @@ var filterednodes: SceneNode[] = [];
 var alphabeticalLabel = false;
 var appendLabel = false;
 
+const setNameList = async () => { nameList = await figma.clientStorage.getAsync("FrameNames"); }
+
 function convertToLetter(n: number) {
   if (n < 27) { let letter = (n + 9).toString(36).toUpperCase(); return letter; }
   else { let letterWrap = n - 26; return convertToLetter(letterWrap); }
@@ -15,14 +17,14 @@ function checkNames(n: String) { for (const name in nameList) { if (n == nameLis
 function resetArrays() { nodes = []; filterednodes = []; }
 
 function selectionFilters(n: SceneNode) { // Finds frames/instances with a listed name & pushes it to nodes[]
-  if ((n.type === "FRAME" || n.type === "INSTANCE") && n.children) {
+  if ((n.type === "FRAME" || n.type === "INSTANCE" || n.type === "SECTION") && n.children) {
     if (checkNames(n.name)) { nodes.push(n); }
     else { for (const c of n.children) { selectionFilters(c); } }
   }
 }
 
 function nodeFilters(n: SceneNode) { // Finds first text child in node and pushes it to filteredNodes[]
-  if ((n.type === "FRAME" || n.type === "INSTANCE") && n.children) {
+  if ((n.type === "FRAME" || n.type === "INSTANCE" || n.type === "SECTION") && n.children) {
     let firstTextChild = n.findOne(x => x.type === "TEXT" && x.visible);
     if (firstTextChild != null) { filterednodes.push(firstTextChild); }
   }
@@ -43,46 +45,47 @@ function positionSort(a: any, b: any) { // evalutates which node is either above
   return 0;
 }
 
-const setNameList = async () => { nameList = await figma.clientStorage.getAsync("FrameNames"); }
-
 const checkSelection = async () => { // Filters initial selection for frames and instances that meet requirements
   setNameList();
-  if (figma.currentPage.selection.length < 1) { figma.ui.postMessage(false); }
-  for (const n of figma.currentPage.selection) { selectionFilters(n); }
-  nodes.sort(positionSort);
-  for (const n of nodes) { nodeFilters(n); }
+  // Checks to make sure something has been selected first
+  if (figma.currentPage.selection.length < 1) { figma.ui.postMessage("None Selected"); }
+  else {
+    for (const n of figma.currentPage.selection) { selectionFilters(n); }
+    nodes.sort(positionSort);
+    for (const n of nodes) { nodeFilters(n); }
 
-  console.log(nodes);
-  console.log(filterednodes);
-
-  if (filterednodes.length > 0) {
-    let count = 0;
-    for (let node of filterednodes) {
-      count++;
-      if (node.type === "TEXT") {
-        await Promise.all(node.getRangeAllFontNames(0, node.characters.length).map(figma.loadFontAsync)); // Loads fonts to be used
-        if (appendLabel) {
-          if (alphabeticalLabel) { node.characters = node.characters + convertToLetter(1 * count); }
-          else { node.characters = node.characters + (1 * count).toString(); /*replace with original string + 1..*/ }
+    if (filterednodes.length > 0) {
+      let count = 0;
+      for (let node of filterednodes) {
+        count++;
+        if (node.type === "TEXT") {
+          await Promise.all(node.getRangeAllFontNames(0, node.characters.length).map(figma.loadFontAsync)); // Loads fonts to be used
+          if (appendLabel) {
+            if (alphabeticalLabel) { node.characters = node.characters + convertToLetter(1 * count); }
+            else { node.characters = node.characters + (1 * count).toString(); /*replace with original string + 1..*/ }
+          }
+          else if (alphabeticalLabel) {
+            node.characters = convertToLetter(1 * count) + "";
+          } else {
+            node.characters = (1 * count).toString(); /*replaces with 1..*/
+            // node.characters = (1 * count).toString().padStart(2, '0'); //replace with 01..
+          }
         }
-        else if (alphabeticalLabel) {
-          node.characters = convertToLetter(1 * count) + "";
-        } else {
-          node.characters = (1 * count).toString(); /*replaces with 1..*/
-        }
-        // node.characters = (1 * count).toString().padStart(2, '0'); //replace with 01..
       }
+      figma.notify("Screen labels have been renumbered!");
+      figma.ui.postMessage("Complete");
+      figma.commitUndo();
+    } else {
+      figma.ui.postMessage("None Found"); // No nodes match the provided label names
+      figma.notify("No matching labels found. Please check layer names.");
     }
-    figma.notify("Screen labels have been renumbered!");
-    figma.ui.postMessage("Complete");
-    figma.commitUndo();
   }
 }
 
 const clearClientStorage = async () => {
   nameList = [];
-  await figma.clientStorage.setAsync("FrameNames", [])
-  figma.ui.postMessage(await figma.clientStorage.getAsync("FrameNames"));
+  await figma.clientStorage.setAsync("FrameNames", []);
+  figma.ui.postMessage("Names Cleared");
 }
 
 const addNewName = async () => {
